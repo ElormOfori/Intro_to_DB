c5fe43135d2a99efcddcbc54e189e175d8d7e765
Elorm Ofori <Ofori.elorm@gmail.com>
2025-02-03 07:29:28 +0000

    Fixed full description query for Books table

--- task_4.sql
+++ task_4.sql
@@ -1,8 +1,8 @@
 -- Ensure we are using the correct database
 USE alx_book_store;
 
--- Query to print full description of the books table
+-- Query to print full description of the Books table
 SELECT COLUMN_NAME, COLUMN_TYPE, IS_NULLABLE, COLUMN_KEY, EXTRA
 FROM INFORMATION_SCHEMA.COLUMNS
-WHERE TABLE_SCHEMA = DATABASE() AND TABLE_NAME = 'books';
+WHERE TABLE_SCHEMA = 'alx_book_store' AND TABLE_NAME = 'Books';
 
